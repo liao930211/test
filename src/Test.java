837479c7d2a3bad75d4fa180c989373e60d1c62e
liao930211 <liao930211@163.com>
2015-05-31 20:15:35 +0800

    加入日期

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,13 @@
+import java.util.Date;
+
 /**
  * Created by Administrator on 2015/5/31.
  */
 public class Test {
 
     public static  void  main(String[] args){
-        System.out.println("123");
+
+
+        System.out.println("123"+new Date());
     }
 }
